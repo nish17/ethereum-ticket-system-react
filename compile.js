12295b005b5a3c51ed55e8d2b3bf98aeedb724cf
nish17 <nimesh3536@gmail.com>
2018-10-17 21:19:24 +0530

    move file

--- compile.js
+++ compile.js
@@ -2,7 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const solc = require("solc");
 
-const Route = path.resolve(__dirname, "contracts", "Route.sol");
+const Route = path.resolve(__dirname, "", "Route.sol");
 const source = fs.readFileSync(Route, "utf-8");
 // console.log(solc.compile(source, 2));
 module.exports = {
